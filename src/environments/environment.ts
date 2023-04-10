98b685497fad4bc0d4d4cd312dba7704cf5997c4
Juangmcz <juangui-089@hotmail.com>
2023-04-10 15:22:50 -0500

    Environments configurations were added.

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'freedom-route-auth',
+    appId: '1:896886985326:web:ec2a515e7eb68e0e74cc7f',
+    storageBucket: 'freedom-route-auth.appspot.com',
+    apiKey: 'AIzaSyAQxvotCi6XGSZvFizjWUg4X31zm9Ry1j4',
+    authDomain: 'freedom-route-auth.firebaseapp.com',
+    messagingSenderId: '896886985326',
+  }
+};

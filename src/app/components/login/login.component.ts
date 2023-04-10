8dc7e0424a5847433f83a04e4fdfc2d7b16078c6
Juangmcz <juangui-089@hotmail.com>
2023-04-10 15:12:24 -0500

    Login-form was implemented.

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user-service/user.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private router: Router, private userService: UserService) {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,}$'),
+      ]),
+    });
+  }
+
+  ngOnInit() {}
+
+  onSubmit() {
+    this.userService
+      .login(this.loginForm.value)
+      .then((response) => {
+        console.log(response);
+        this.router.navigate(['/main']);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  onClick() {
+    this.userService
+      .loginWithGoogle()
+      .then((response) => {
+        console.log(response);
+        this.router.navigate(['/main']);
+      })
+      .catch((error) => console.log(error));
+  }
+}

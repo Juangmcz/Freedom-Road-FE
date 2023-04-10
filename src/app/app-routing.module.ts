675d7b2ddceb540a944bdfc9da65aa56dda527b2
Juangmcz <juangui-089@hotmail.com>
2023-04-10 18:53:22 -0500

    Forms folder was added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './components/forms/login/login.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './components/forms/register/register.component';
 import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 const routes: Routes = [

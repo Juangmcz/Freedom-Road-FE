cd5b1cf7bbc99db85cef4425d67054d9b760759e
Juangmcz <juangui-089@hotmail.com>
2023-04-10 14:03:46 -0500

    Some paths were implemented.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { RegisterComponent } from './components/register/register.component';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  {
+    path: 'main',
+    component: HomepageComponent,
+    ...canActivate(() => redirectUnauthorizedTo(['/register'])),
+  },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

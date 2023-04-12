48ee4a95ca282dcc509089d64b0ccf7c183c1dd8
Juangmcz <juangui-089@hotmail.com>
2023-04-11 20:28:56 -0500

    Type reference was added.

--- src/app/services/customer-service/customer.service.ts
+++ src/app/services/customer-service/customer.service.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Customer } from 'src/app/models/customer.model';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,7 @@ export class CustomerService {
 
   api: string = 'http://localhost:8080/api/customers';
 
-  getAll() {
+  getAll(): Observable<any> {
     return this.http.get(this.api);
   }
 
@@ -22,11 +23,11 @@ export class CustomerService {
     return this.http.get(`${this.api}/email/${email}`);
   }
 
-  save(customer: any): Observable<any> {
+  save(customer: Customer): Observable<any> {
     return this.http.post(this.api, customer);
   }
 
-  update(id: string, customer: any): Observable<any> {
+  update(id: string, customer: Customer): Observable<any> {
     return this.http.put(`${this.api}/${id}`, customer);
   }
 

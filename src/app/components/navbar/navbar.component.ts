91f1b28007ac4b01155b24c3a44a8d3b9ecb091f
Juangmcz <juangui-089@hotmail.com>
2023-04-12 15:23:59 -0500

    New methods were added.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { faRoute } from '@fortawesome/free-solid-svg-icons';
+import { UserService } from 'src/app/services/user-service/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  faRoute = faRoute;
 
+  constructor(private userService: UserService, private router: Router) {}
+
+  isLoggedIn(): boolean {
+    console.log('the state is:' + this.userService.getState());
+    return this.userService.getState();
+  }
+
+  logout(): void {
+    this.userService
+      .logout()
+      .then(() => {
+        this.router.navigate(['/login']);
+      })
+      .catch((error) => console.log(error));
+  }
 }

703d9905011b0486cb873dba4763c2ae144689f4
Juangmcz <juangui-089@hotmail.com>
2023-04-13 11:45:48 -0500

    AddToCart method was implemented.

--- src/app/components/bus-tickets/bus-ticket/bus-ticket.component.ts
+++ src/app/components/bus-tickets/bus-ticket/bus-ticket.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { BusTicket } from 'src/app/models/bus-ticket.model';
+import { UserService } from 'src/app/services/user-service/user.service';
 
 @Component({
   selector: 'app-bus-ticket',
@@ -16,4 +18,24 @@ export class BusTicketComponent {
     price: 0,
     numberOfTickets: 0,
   };
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  addToCart() {
+    let tickets: any[] = JSON.parse(localStorage.getItem('items') || '[]');
+
+    if (!this.userService.getState()) {
+      this.router.navigate(['/login']);
+    }
+
+    if (
+      !(
+        tickets?.filter((ticket) => ticket.origin === this.busTicket.origin)
+          .length > 0
+      )
+    ) {
+      tickets?.push(this.busTicket);
+      localStorage.setItem('items', JSON.stringify(tickets));
+    }
+  }
 }
